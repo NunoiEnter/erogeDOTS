6e533fe6b1d2c11f1c39c62224ae8b6547134d3c
NunoiEnter <nunoiauto3749@gmail.com>
2026-07-22 11:23:54 +0700

    fix: services.flatpak not programs.flatpak

--- hosts/NixChan/configuration.nix
+++ hosts/NixChan/configuration.nix
@@ -27,7 +27,7 @@
 
   programs.niri.enable = true;
   programs.zsh.enable = true;
-  programs.flatpak.enable = true;
+  services.flatpak.enable = true;
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
 
